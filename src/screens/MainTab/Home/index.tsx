f459d958c37cb475c76831004e0c57793e0bf472
Đỗ Thành Đạt <panda.snkrhead273@gmail.com>
2022-01-07 00:54:07 +0700

    xin_loi_quocHieu

--- src/screens/MainTab/Home/index.tsx
+++ src/screens/MainTab/Home/index.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 const Home = () => {
   return (
-    <SafeAreaView>
-      <Text>xin lỗi quốc hiếu</Text>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1, marginTop: 16, alignItems: 'center'}}>
+        <Text style={{fontSize: 30, fontWeight: 'bold'}}>
+          Lung Cancer Care System
+        </Text>
+
+        <Image
+          source={require('../../../assets/LungCACXR.png')}
+          style={{height: 350, width: 350, marginTop: 16}}
+        />
+      </View>
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Text style={{fontSize: 20, marginHorizontal: 16}}>
+          According to recent statistics, lung cancer is the disease that
+          accounts for the highest rate of morbidity and mortality among cancer
+          diseases globally.
+        </Text>
+        <TouchableOpacity
+          style={{
+            backgroundColor: 'white',
+            height: 63,
+            width: 350,
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 30,
+            alignSelf: 'center',
+            marginTop: 42,
+          }}>
+          <Text style={{fontSize: 16}}>View More</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
