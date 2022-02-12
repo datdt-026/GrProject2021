dc3d606b8951ce6706053e0b13cee6d57b8fab05
Đỗ Thành Đạt <panda.snkrhead273@gmail.com>
2022-02-12 23:23:04 +0700

    chat_left

--- src/nav/RootStack.tsx
+++ src/nav/RootStack.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 import MainTab from './MainTab';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import WelcomeScreen from '../screens/Start';
 import SignIn from '../screens/Start/SignIn';
 import SignUp from '../screens/Start/SignUp';
@@ -31,17 +31,17 @@ const RootStack = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
+        {/* <Stack.Screen
           name="WelcomeScreen"
           component={WelcomeScreen}
           options={{ headerShown: false }}
-        />
+        /> */}
         <Stack.Screen
           name="MainTab"
           component={MainTab}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
-        <Stack.Screen
+        {/* <Stack.Screen
           name="SignIn"
           component={SignIn}
           options={{ headerShown: false }}
@@ -50,26 +50,26 @@ const RootStack = () => {
           name="SignUp"
           component={SignUp}
           options={{ headerShown: false }}
-        />
+        /> */}
         <Stack.Screen
           name="Doctor"
           component={Doctor}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="Hospitals"
           component={Hospitals}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="List"
           component={List}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="Arcticle"
           component={Arcticle}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="Upload"
