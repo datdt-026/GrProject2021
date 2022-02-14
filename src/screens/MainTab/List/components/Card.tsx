8fd443615cef963c1502146c22282b117ad1c7df
itsThai <khangthai276@gmail.com>
2022-02-14 22:29:32 +0700

    patient

---
+++ src/screens/MainTab/List/components/Card.tsx
@@ -0,0 +1,112 @@
+import {
+    Dimensions,
+    FlatList,
+    Image,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    StatusBar
+  } from 'react-native';
+  import React, {useState, useEffect} from 'react';
+  import SearchComponent from '../../../../components/Search';
+  import axios from "axios";
+  
+  const Card = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+    const [data, setData] = useState([]);
+    const [err, setErr] = useState("");
+    const [term, setTerm] = useState("");
+  
+    const getData = (name) => {
+      axios.get(`https://61a718ea8395690017be94dc.mockapi.io/Patients?name=${name}`).then((res) => {
+        if (res.data.length > 0) {
+          setData(res.data);
+        } else {
+          setData([]);
+          setErr("                                     No patient found");
+        }
+      });
+    };
+    useEffect(() => {
+      getData(term);
+    }, [term]);
+  
+  
+    const renderCard = ({ item }) => {
+        return (
+          <TouchableOpacity>
+              <View style={{margin: 12}}>
+                  <Image
+                      style={{
+                      width: 240,
+                      height: 200,
+                      borderRadius: 10,
+                      borderColor: 'black',
+                      }}
+                      source={{uri: `${item.src}`}}
+                  />
+                  <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
+                      - ID: {item.id}
+                  </Text>
+                  <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
+                      - Name: {item.name}
+                  </Text>
+                  <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
+                      - Age: {item.age}
+                  </Text>
+                  <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
+                      - Address: {item.address}
+                  </Text>
+                  <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
+                      - Phone: {item.phone}
+                  </Text>
+                  <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
+                      - Symptoms: {item.sympstoms}
+                  </Text>
+                  <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
+                      - Date of Diagnosis: {item.date_diagnosis}
+                  </Text>
+                  <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
+                      - Date of CT Scan: {item.date_ctscan}
+                  </Text>
+                  <Text
+                      style={{
+                      color: '#A1A4B2',
+                      marginTop: 5,
+                      textAlign: 'left',
+                  }}>
+                      _______________
+                  </Text>
+              </View>
+          </TouchableOpacity>
+        )
+      };
+  
+    return (
+      <>
+        <View>
+          <SearchComponent onSearchEnter={(newTerm) => {
+            setTerm(newTerm);
+            setErr("");
+          }} />
+  
+          {err ?
+            <Text>{err}</Text>
+            :
+            <FlatList
+              data={data}
+              renderItem={renderCard}
+              keyExtractor={Patient => Patient.name}
+            />
+          }
+        </View>
+      </>
+    );
+  };
+  
+  export default Card;
+  
+  const styles = StyleSheet.create({});
+  
--- src/screens/MainTab/List/index.tsx
+++ src/screens/MainTab/List/index.tsx
@@ -10,86 +10,19 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import urls from '../../../config/Api';
 import Header from './components/header';
-
-import {IPatient} from '../../../data/information';
+import Card from './components/Card';
 
 const List = () => {
-  const [data, setData] = React.useState<IPatient[]>([]);
-  const [loading, setLoading] = React.useState<boolean>(true);
-
-  React.useEffect(() => {
-    fetch(urls.patient)
-      .then(response => response.json())
-      .then(patients => {
-        console.log('json', patients);
-        setData(patients);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
-
+  
   return (
     <SafeAreaView style={{flex: 1}}>
       <Header />
-      <View style={{flex: 1}}>
-        <FlatList
-          data={data}
-          keyExtractor={({id}, index) => id.toString()}
-          renderItem={({item}) => (
-            <View style={{margin: 12}}>
-              <Image
-                style={{
-                  width: 240,
-                  height: 200,
-                  borderRadius: 10,
-                  borderColor: 'black',
-                }}
-                source={{uri: `${item.src}`}}
-              />
-              <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
-                - ID: {item.id}
-              </Text>
-              <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
-                - Name: {item.name}
-              </Text>
-              <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
-                - Age: {item.age}
-              </Text>
-              <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
-                - Address: {item.address}
-              </Text>
-              <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
-                - Phone: {item.phone}
-              </Text>
-              <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
-                - Sympstoms: {item.sympstoms}
-              </Text>
-              <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
-                - Date of Diagnosis: {item.date_diagnosis}
-              </Text>
-              <Text style={{color: '#A1A4B2', marginTop: 5, textAlign: 'left'}}>
-                - Date of CT Scan: {item.date_ctscan}
-              </Text>
-              <Text
-                style={{
-                  color: '#A1A4B2',
-                  marginTop: 5,
-                  textAlign: 'left',
-                }}>
-                _______________
-              </Text>
-            </View>
-          )}
-        />
-      </View>
+
+      <Card />
     </SafeAreaView>
   );
 };
-
 export default List;
 
 const styles = StyleSheet.create({
